b4c23ff1ddd8cb0d7cc43f518648e231f3a6fbe7
Nexora <kitchi734@gmail.com>
2025-06-04 12:30:26 +0800

    feat: completed portfolio detail page

--- src/app/portfolio/[n]/page.jsx
+++ src/app/portfolio/[n]/page.jsx
@@ -1,9 +1,24 @@
-const PortfolioPage = () => {
+import ClientPortfolioPage from "@/app/portfolio/[n]/ClientPortfolioPage";
+import { ClientsSection, TestimonialsSection } from "@/components";
+import { portfolioItems } from "@/data/PortfolioItems";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  return portfolioItems.map(({ id }) => ({ n: id.toString() }));
+}
+
+export default async function PortfolioPage({ params }) {
+  const { n: projectId } = await params;
+
+  if (!portfolioItems.some(({ id }) => id.toString() === projectId)) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className="pt-32">
+      <ClientPortfolioPage n={projectId} />
+      <ClientsSection />
+      <TestimonialsSection />
     </div>
   );
-};
-
-export default PortfolioPage;
+}
